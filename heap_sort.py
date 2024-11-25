e9b48f82d8d62ddddfd677c6273b9e9386f2b5e6
Anton Radzionau <radionoff.antony@gmail.com>
2024-11-25 14:10:41 +0300

    Fix heapify and heapSort functions for correct heap construction and sorting

--- heap_sort.py
+++ heap_sort.py
@@ -4,7 +4,7 @@ def heapify(arr, n, i):
     largest = i
     l = 2 * i + 1
     r = 2 * i + 2
-    if l < n and arr[i] < arr[l]:
+    if l < n and arr[largest] < arr[l]:
         largest = l
     if r < n and arr[largest] < arr[r]:
         largest = r
@@ -14,7 +14,7 @@ def heapify(arr, n, i):
 
 def heapSort(arr):
     n = len(arr)
-    for i in range(n, -1, -1):
+    for i in range(n // 2 - 1, -1, -1):
         heapify(arr, n, i)
     for i in range(n-1, 0, -1):
         arr[i], arr[0] = arr[0], arr[i]
